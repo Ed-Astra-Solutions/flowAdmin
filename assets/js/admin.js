dec4794c7b1e1610f2ecbb5c38e54c37fdbcdd10
edtech-hub <158812683+edtech-hub@users.noreply.github.com>
2026-05-08 01:50:36 +0530

    Add files via upload

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1287,13 +1287,13 @@ function renderReviewsList() {
                     <div class="review-date">${date}</div>
                 </div>
                 <div class="review-actions">
-                    <button onclick="openEditReviewModal('${review._id}')" title="Edit">
+                    <button type="button" onclick="openEditReviewModal('${review._id}')" title="Edit">
                         <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <path d="M11 4H4a2 2 0 00-2 2v14a2 2 0 002 2h14a2 2 0 002-2v-7"/>
                             <path d="M18.5 2.5a2.121 2.121 0 013 3L12 15l-4 1 1-4 9.5-9.5z"/>
                         </svg>
                     </button>
-                    <button class="delete" onclick="deleteReview('${review._id}')" title="Delete">
+                    <button type="button" class="delete" onclick="deleteReview('${review._id}')" title="Delete">
                         <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                             <polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
                         </svg>
